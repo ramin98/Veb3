9d6b2e869902aa62470e6f6e9f6aec1f92aae307
ramin98 <59912261+ramin98@users.noreply.github.com>
2024-03-14 03:21:00 -0700

    Update tapshiriqlar.js

--- tapshiriqlar.js
+++ tapshiriqlar.js
@@ -11,3 +11,33 @@ for (let index = 0; index < input.length; index++) {
 }
 
 console.log(newText)
+
+//CLOSURE
+
+const myFunc = () => {
+    let i = 0
+    return () => {
+        i++
+        return i
+    }
+}
+
+let f = myFunc()
+
+console.log(f())
+console.log(f())
+console.log(f())
+console.log(f())
+
+
+// NORMAL FUNCTION ISHLEMEYECEK
+const myFunc2 = () => {
+    let i = 0
+    i++
+    return i
+}
+
+console.log(myFunc2())
+console.log(myFunc2())
+console.log(myFunc2())
+console.log(myFunc2())
