dd6b58f2dad4e15f00b4aa09801222efc656c415
ramin98 <59912261+ramin98@users.noreply.github.com>
2024-04-22 06:36:23 -0700

    Update script.js

--- work with api/script.js
+++ work with api/script.js
@@ -1,4 +1,36 @@
 let list = document.querySelector('#list')
+let form = document.querySelector('#addForm')
+let changeFormContainer = document.querySelector('#changeFormContainer')
+let changeForm = document.querySelector('#changeForm')
+let idOfChangingElement
+
+const addItem = async (event) => {
+    try {
+    event.preventDefault()
+    console.log('work')
+    let formData = new FormData(form)
+    let obj = {
+        from: formData.get('from'),
+        to: formData.get('to'),
+        message: formData.get('message')
+    }
+    
+        let res = await fetch('http://149.100.156.6:3000/items/', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(obj)
+        })
+        if (res.ok === true) {
+            getFetch()
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+form.addEventListener('submit', addItem)
 
 const deleteItems = async (id) => {
     try {
@@ -13,6 +45,42 @@ const deleteItems = async (id) => {
         console.log(error)
     }
 }
+
+const openModal =  (id) => {
+    changeFormContainer.style = 'display:flex'
+    idOfChangingElement = id
+}
+
+const changeItem = async (event) => {
+    try {
+    event.preventDefault()
+    console.log('work')
+    let formData = new FormData(changeForm)
+    let obj = {
+        from: formData.get('from'),
+        to: formData.get('to'),
+        message: formData.get('message')
+    }
+    
+        let res = await fetch(`http://149.100.156.6:3000/items/${idOfChangingElement}`, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(obj)
+        })
+        if (res.ok === true) {
+            getFetch()
+        }
+    } catch (error) {
+        console.log(error)
+    }
+    changeFormContainer.style = 'display:none'
+
+}
+
+changeForm.addEventListener('submit', changeItem)
+
 const getFetch = async () => {
     try {
         const res = await fetch('http://149.100.156.6:3000/items/')
@@ -23,7 +91,8 @@ const getFetch = async () => {
                 let li = document.createElement('li')
                 li.innerHTML = `<span>${element.from}-</span><span>${element.to}-</span>
                 <span>${element.message}-</span><span>${element.id}-</span>
-                <button onclick="deleteItems(${element.id})">X</button>`
+                <button onclick="deleteItems(${element.id})">X</button>
+                <button onclick="openModal(${element.id})">CHANGE</button>`
                 list.appendChild(li)
             });
         }
@@ -33,3 +102,4 @@ const getFetch = async () => {
 }
 
 getFetch()
+
