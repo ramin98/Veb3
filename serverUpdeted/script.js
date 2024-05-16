1ec41931f4868d621f7ad3358029c1fe331d7880
ramin98 <59912261+ramin98@users.noreply.github.com>
2024-05-16 06:34:12 -0700

    Update script.js

--- serverUpdeted/script.js
+++ serverUpdeted/script.js
@@ -1,58 +1,70 @@
 let list = document.querySelector('#list')
-let userValue = document.querySelector('#user-name')
+let fromUser = document.querySelector('#from-user-name')
+let toUser = document.querySelector('#to-user-name')
 let changeValue = document.querySelector('#change-name')
 let form = document.querySelector('#add-form')
+let search = document.querySelector('#search')
 let changeForm = document.querySelector('#change-form')
 
 let userId
-function showUsers(){
+
+search.addEventListener('click', () => {
+    let searchInputFrom = document.querySelector('#searchInputFrom').value
+    let searchInputTo = document.querySelector('#searchInputTo').value
+    fetch(`http://localhost:5000/search?from=${searchInputFrom}&to=${searchInputTo}`)
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+})
+
+function showUsers() {
     fetch('http://localhost:5000/workers')
-    .then((res) => res.json())
-    .then(data => {
-        data.forEach(element => {
-            let li = document.createElement('li')
-            li.innerHTML = `
+        .then((res) => res.json())
+        .then(data => {
+            data.forEach(element => {
+                let li = document.createElement('li')
+                li.innerHTML = `
             <div>
             <span>${element.id}</span>
-            <span>${element.userName}</span>
+            <span>${element.from}</span>
+            <span>${element.to}</span>
             <button onclick="deleteUser('${element.id}')">X</button>
             <button onclick="findUserId('${element.id}')">Change</button>
             </div>`
-            list.appendChild(li)
-        });
-    })
+                list.appendChild(li)
+            });
+        })
 }
 
 showUsers()
 
-function deleteUser(id){
-      fetch(`http://localhost:5000/delete-user/${id}`, {
-        method:'DELETE'
-      }).then((res) => {
-        if(res.ok){
+function deleteUser(id) {
+    fetch(`http://localhost:5000/delete-user/${id}`, {
+        method: 'DELETE'
+    }).then((res) => {
+        if (res.ok) {
             showUsers()
         }
-      })
+    })
 }
 
-function findUserId(id){
+function findUserId(id) {
     userId = id
     let changeContainer = document.querySelector('#change-container')
     changeContainer.style = 'display:flex;'
 }
 
-function changeUser(ev){
+function changeUser(ev) {
     ev.preventDefault()
     fetch(`http://localhost:5000/change-user/${userId}`, {
-      method:'PUT',
-      headers:{
-        'Content-type': 'application/json'
-    },
-    body: JSON.stringify({userName:changeValue.value})
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({ userName: changeValue.value })
     }).then((res) => {
-      if(res.ok){
-          showUsers()
-      }
+        if (res.ok) {
+            showUsers()
+        }
     })
     let changeContainer = document.querySelector('#change-container')
     changeContainer.style = 'display:none;'
@@ -64,11 +76,12 @@ changeForm.addEventListener('submit', changeUser)
 form.addEventListener('submit', (ev) => {
     ev.preventDefault()
     let object = {
-        userName:userValue.value
+        from: fromUser.value,
+        to: toUser.value
     }
-    fetch('http://localhost:5000/add-user',{
-        method:'POST',
-        headers:{
+    fetch('http://localhost:5000/add-user', {
+        method: 'POST',
+        headers: {
             'Content-type': 'application/json'
         },
         body: JSON.stringify(object)
